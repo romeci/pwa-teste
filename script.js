a874c8365a28d0dce7228fa429bb4ba10f246d28
Romeci Santos <git@romeci.com.br>
2024-05-13 18:42:14 -0300

    Atualizado em: Seg 13 Mai 2024 18:42:14 -03

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
+      navigator.serviceWorker.register('service-worker.js?v=2')
           .then((registration) => {
               console.log('Service Worker registrado com sucesso!', registration);
           })
