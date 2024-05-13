e1c615c966d7b0172598f0e48e480d7f7be68783
Romeci Santos <git@romeci.com.br>
2024-05-13 18:47:40 -0300

    Atualizado em: Seg 13 Mai 2024 18:47:40 -03

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js?v=3')
+      navigator.serviceWorker.register('service-worker.js?v=4')
           .then((registration) => {
               console.log('Service Worker registrado com sucesso!', registration);
           })
@@ -10,47 +10,24 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// window.addEventListener('beforeinstallprompt', (event) => {
-//   event.preventDefault();
-//   const promptEvent = event;
-
-//   // Exibir mensagem nativa para instalar o aplicativo
-//   if (confirm('Você gostaria de instalar o aplicativo?')) {
-//       promptEvent.prompt();
-//       promptEvent.userChoice.then((choiceResult) => {
-//           if (choiceResult.outcome === 'accepted') {
-//               console.log('Usuário aceitou instalar o aplicativo');
-//           } else {
-//               console.log('Usuário rejeitou instalar o aplicativo');
-//           }
-//       });
-//   }
-// });
-
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   const promptEvent = event;
 
   // Exibir mensagem nativa para instalar o aplicativo
   if (promptEvent.prompt) {
-      if (confirm('Você gostaria de instalar o aplicativo?')) {
-          promptEvent.prompt();
-          promptEvent.userChoice.then((choiceResult) => {
-              if (choiceResult.outcome === 'accepted') {
-                  alert('Usuário aceitou instalar o aplicativo');
+      promptEvent.prompt();
+      promptEvent.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+              alert('Usuário aceitou instalar o aplicativo');
 
-                  // Download e instalação do aplicativo
-                  promptEvent.userChoice.then(() => {
-                      alert('Iniciando download e instalação do aplicativo');
-                  });
-              } else {
-                  alert('Usuário rejeitou instalar o aplicativo');
-              }
-          });
-      }
+              // Download e instalação do aplicativo
+              alert('Iniciando download e instalação do aplicativo');
+          } else {
+              alert('Usuário rejeitou instalar o aplicativo');
+          }
+      });
   } else {
       alert('Para instalar o aplicativo, acesse as configurações do navegador.');
   }
 });
-
-
