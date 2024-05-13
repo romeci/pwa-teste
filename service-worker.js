9502e054f8537ddb7678c2c1516c57eda5c2d202
Romeci Santos <git@romeci.com.br>
2024-05-13 19:23:23 -0300

    Atualizado em: Seg 13 Mai 2024 19:23:23 -03

--- service-worker.js
+++ service-worker.js
@@ -6,37 +6,6 @@ self.addEventListener('activate', (event) => {
   console.log('Service Worker ativado');
 });
 
-self.addEventListener('beforeinstallprompt', (event) => {
-  // Exibir prompt de instalação nativo
-  event.preventDefault();
-  event.prompt();
-
-  // Armazenar o evento para uso posterior
-  const promptEvent = event;
-
-  // Tratar a instalação do app
-  self.addEventListener('install', (event) => {
-    // Comparar os IDs de evento para confirmar que a instalação foi iniciada pelo prompt
-    if (event.installing.id === promptEvent.eventId) {
-      // Esperar a instalação ser concluída
-      event.installing.addEventListener('statechange', (event) => {
-        if (event.target.state === 'installed') {
-          // Instalação concluída!
-          console.log('App instalado com sucesso!');
-
-          // Disparar um evento customizado para notificar a página sobre a instalação
-          self.dispatchEvent(new Event('appInstalled'));
-        }
-      });
-    }
-  });
-});
-
-// Ouvir o evento customizado 'appInstalled' na página principal
-window.addEventListener('appInstalled', () => {
-  // Exibir mensagem de sucesso ou realizar outras ações após a instalação
-  console.log('App instalado! ');
-});
 
 
 self.addEventListener('fetch', (event) => {
