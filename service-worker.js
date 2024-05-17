c9b1f7b45c76e85c4184b2d467e2072d26949b89
Romeci Santos <git@romeci.com.br>
2024-05-17 12:00:43 -0300

    Atualizado em: Sex 17 Mai 2024 12:00:42 -03

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,47 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker ativado');
-});
+// self.addEventListener('install', (event) => {
+//   console.log('Service Worker instalado');
+// });
 
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
+// self.addEventListener('activate', (event) => {
+//   console.log('Service Worker ativado');
+// });
 
 self.addEventListener('fetch', (event) => {
   console.log('Requisição de rede:', event.request.url);
   event.respondWith(
-      fetch(event.request, { cache: 'no-store' }) // Evitar o cache
+      caches.match(event.request)
+          .then((response) => {
+              return response || fetch(event.request);
+          })
   );
 });
